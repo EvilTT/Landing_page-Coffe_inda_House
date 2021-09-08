dac6792466672bbc0890ea4feb224808ce8076e3
Maxim Zavadskiy <mega.zava199@gmail.com>
2021-09-08 18:43:00 +0300

    Configured form validation with highlighting of incorrectly filled fields. All fonts are plugged in via the font-fase rule. Added animation for submitting the form.

--- src/js/feedback-form.js
+++ src/js/feedback-form.js
@@ -4,42 +4,130 @@ const labelOfContact = document.querySelector('.contact-text')
 const fullName = document.querySelectorAll('input[type=text]')
 const textarea = document.querySelector('TEXTAREA')
 
+const removeError = (field) => field.classList.remove('_error-validation')
+
 const clearInputInForm = () => {
-    [...fullName, textarea, inputOfContact].forEach(item => item.value = '')
+    ;[...fullName, textarea, inputOfContact].forEach((item) =>
+        removeError(item)
+    )
+    form.reset()
 }
 
+const generateModal = () => {
+    let modal = document.createElement('div')
+}
 
 form.addEventListener('focusout', (event) => {
-    if (!event.target.classList.contains('input')) return
+    if (!event.target.classList.contains('fullName')) return
     event.target.value !== ''
-        ? event.target.parentNode.lastElementChild.id = 'textInInput'
-        : event.target.parentNode.lastElementChild.id = ''
+        ? (event.target.parentNode.lastElementChild.id = 'textInInput')
+        : (event.target.parentNode.lastElementChild.id = '')
 })
 
 form.addEventListener('change', (event) => {
-    if(!(event.target.name === 'contact')) return
-    switch(event.target.value){
-        case 'telephone': 
+    if (!(event.target.name === 'contact')) return
+    switch (event.target.value) {
+        case 'telephone':
             inputOfContact.setAttribute('type', 'telephone')
             labelOfContact.innerHTML = 'Телефон'
-        break
-        case 'email': 
+            break
+        case 'email':
             inputOfContact.setAttribute('type', 'email')
             labelOfContact.innerHTML = 'E-mail'
-        break
+            break
     }
 })
 
-form.addEventListener('click', function(event){
-    if(event.target.classList.contains('clear')){
+form.addEventListener('click', function (event) {
+    if (event.target.classList.contains('clear')) {
         event.preventDefault()
-        clearInputInForm(  )
+        clearInputInForm()
         return
     }
-    if(event.target.classList.contains('submit')){
+    if (event.target.classList.contains('submit')) {
         event.preventDefault()
-        form.submit()
-        
+        formValidation()
     }
 })
 
+const validateEmail = (email) =>
+    /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(email)
+
+const validateTel = (tel) => /\+375\d{9}$/.test(tel)
+
+const validateFullname = (valueOfFullName) =>
+    valueOfFullName.length >= 3 &&
+    valueOfFullName.length <= 15 &&
+    valueOfFullName !== ''
+        ? true
+        : false
+
+const validateError = (errorsFiel) => {
+    errorsFiel.classList.add('_error-validation')
+    errorsFiel.focus()
+}
+
+// const toMail = async (dataForm) => {
+//     let data = new FormData(dataForm)
+//     let response = await fetch('mail.php', {
+//         body: data,
+//         method: 'POST',
+//     })
+//     if(response.ok) document.body.classList.remove('send')   
+// }
+
+const formValidation = () => {
+    let requiredElements = document.querySelectorAll('[data-req=req]')
+    let errors = 0
+
+    for (let field of requiredElements) {
+        switch (field.name) {
+            case 'name':
+                if (validateFullname(field.value)) {
+                    removeError(field)
+                } else {
+                    validateError(field)
+                    errors++
+                }
+                break
+            case 'surname':
+                if (validateFullname(field.value)) {
+                    removeError(field)
+                } else {
+                    validateError(field)
+                    errors++
+                }
+                break
+
+            case 'contact-info':
+                switch (field.type) {
+                    case 'email':
+                        if (validateEmail(field.value)) {
+                            removeError(field)
+                        } else {
+                            validateError(field)
+                            errors++
+                        }
+                        break
+
+                    case 'telephone':
+                        if (validateTel(field.value)) {
+                            removeError(field)
+                        } else {
+                            validateError(field)
+                            errors++
+                        }
+                        break
+                }
+                break
+
+            case 'privacy-policy':
+                if (!field.checked) errors++
+                break
+        }
+    }
+    if (errors === 0) {
+        document.body.querySelector('.load').style.top = window.pageYOffset + 'px'
+        document.body.classList.add('send')
+    }
+}
