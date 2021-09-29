82c89a392ebebd4b9a5d0d6f6f36c9ad7b5f9ded
Maxim Zavadskiy <mega.zava199@gmail.com>
2021-09-30 00:39:44 +0300

    Edited menu, added room photos. Photo carousel added. Lazy loading for photo carousel.

--- build/js/slider-about.js
+++ build/js/slider-about.js
@@ -11,3 +11,14 @@ const swiperAbout = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+let iconFullScreen = 'fas fa-expand'
+let iconCloseFullScreen = 'fas fa-compress'
+
+document.querySelector('.icon-sreen').addEventListener('click', function(event){
+  this.parentNode.classList.toggle('bigImage')
+  document.body.classList.toggle('fullScreen')
+  
+  let icon = this.querySelector('I')
+  icon.classList.contains('fas fa-expand') ? icon.className = iconCloseFullScreen : icon.className = iconCloseFullScreen
+})
